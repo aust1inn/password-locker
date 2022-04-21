36c43f7cbdfe315e42b8ed0bada0f3a765086268
aust1inn <austinotieno364@gmail.com>
2022-04-21 17:29:30 +0300

    creates find user by name and delete user methods

--- user.py
+++ user.py
@@ -1,3 +1,8 @@
+from unicodedata import name
+
+from importlib_metadata import method_cache
+
+
 class User:
     """""
     Creates user class that will generate users instances
@@ -16,4 +21,26 @@ class User:
 
         """""
 
-        User.user_list.append(self)  
+        User.user_list.append(self)
+
+    @classmethod
+    def find_user_via_name(cls,username):
+        """"" 
+        A method that will find a user by name
+
+        args:username
+
+        """""   
+        for user in cls.user_list:
+            if User.username == username:
+                return user
+
+    def delete_user (self):
+        """""
+        Deletes user from user_list by the remove method
+
+        """""
+        User.user_list.remove(self)
+
+
+        
