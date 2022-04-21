a81810cd6fb6df8989d07e96be399689e4a171b2
aust1inn <austinotieno364@gmail.com>
2022-04-21 14:52:44 +0300

    creates the User blueprint and sets properties

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    """""
+    Creates user class that will generate users instances
+
+    """""
+
+    user_list=[]
+
+    def __init__(self,username,password):
+        self.username=username
+        self.password=password
