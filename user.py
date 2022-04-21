08ce13397830243866463381385d662369536c28
aust1inn <austinotieno364@gmail.com>
2022-04-21 17:14:42 +0300

    sets save_user method on class User

--- user.py
+++ user.py
@@ -9,3 +9,11 @@ class User:
     def __init__(self,username,password):
         self.username=username
         self.password=password
+
+    def save_user(self):
+        """""  
+        Saves user by appending them into the user_list list
+
+        """""
+
+        User.user_list.append(self)  
